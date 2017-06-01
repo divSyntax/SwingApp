4d37c357375bea636d882681161c57cb65964510
divSyntax <divsyntax@gmail.com>
2017-05-31 20:42:12 -0500

    Update FormPanel.java

--- FormPanel.java
+++ FormPanel.java
@@ -6,15 +6,83 @@ import javax.swing.border.Border;
 
 public class FormPanel extends JPanel
 {
+
+	private JLabel nameLabel;
+	private JTextField nameField;
+	private JLabel occupationLabel;
+	private JTextField occupationField;
+	private JButton okBtn;
+
 	public FormPanel()
 	{
 		Dimension dim =  getPreferredSize();
 		dim.width = 300;
 		setPreferredSize(dim);
 		
+		nameLabel = new JLabel("Name:");
+		occupationLabel = new JLabel("Occupation:");
+		nameField = new JTextField(10);
+		occupationField = new JTextField(10);
+		
+		okBtn = new JButton("Ok");
+		
 		Border innerBorder = BorderFactory.createTitledBorder("Add Person");
 		Border outterBorder = BorderFactory.createEmptyBorder(5,5,5,5);
 		setBorder(BorderFactory.createCompoundBorder(outterBorder,innerBorder));
+		
+			
+		//setup layout grid
+		setLayout(new GridBagLayout());
+		
+		GridBagConstraints gc = new GridBagConstraints();
+		
+		
+		
+		////first row///////////////////
+		
+		gc.weightx = 1;
+		gc.weighty = 0.1;
+		
+		gc.gridx = 0;
+		gc.gridy = 0;
+		gc.fill = GridBagConstraints.NONE;
+		gc.anchor = GridBagConstraints.LINE_END;
+		gc.insets = new Insets(0,0,0,5);
+		add(nameLabel, gc);
+		
+		gc.gridx = 1;
+		gc.gridy = 0;
+		gc.anchor = GridBagConstraints.LINE_START;
+		add(nameField, gc);
+		
+		////second row///////////////////
+		gc.weightx = 1;
+		gc.weighty = 0.1;
+		
+		gc.gridx = 0;
+		gc.gridy = 1;
+		gc.anchor = GridBagConstraints.LINE_END;
+		gc.insets = new Insets(0,0,0,5);
+		add(occupationLabel,gc);
+		
+		gc.gridx = 1;
+		gc.gridy = 1;
+		gc.anchor = GridBagConstraints.LINE_START;
+		add(occupationField,gc);
+		
+		////third row///////////////////
+		
+		gc.weightx = 1;
+		gc.weighty = 2.0;
+		
+		gc.gridx = 1;
+		gc.gridy = 2;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		add(okBtn, gc);
+		
+		
+		
+		
 	}
 
 }
